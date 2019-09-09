f12eaf8a3d24f449d22a88b590e56a0f75a189d7
Michael George <georgem@wlwv.k12.or.us>
2019-09-09 08:39:20 -0700

    Just changing things.

---
+++ Jabberwockky.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Jabberwocky here.
+ *
+ * @author Owen Hoy)
+ * @version 1.0
+ */
+public class Jabberwockky
+{
+    public static void main(String[] args);// 
+    System.out.print("'twas brillig, and the slithy toves/"); 
+    System.out.print("\n   'Did gyre and gimble in the wabe:");
+    System.out.print("'All mimsy were the “borogroves”/\n And the mome raths out grabe.");		
+  
+    }
+}
